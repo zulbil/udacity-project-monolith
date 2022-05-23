a22b23011eead4d68368ed3cbbdf28aed94f6d4d
Zulbil <alexkhang25@yahoo.fr>
2022-05-22 22:00:52 -0500

    update api host by using the reverse proxy ip address

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ac29af9e76c6244b9b60b3d48de4293f-2107253137.us-west-2.elb.amazonaws.com'
 };
 
 /*
