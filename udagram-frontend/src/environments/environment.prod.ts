bd45ae812247fd2efa65856e26aa7c3b16b0f2fd
Zulbil <alexkhang25@yahoo.fr>
2022-05-25 16:42:40 -0500

    update environment.prod.ts

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aeaee4199c32447cb9c1fa8d80ab2a8b-159746184.us-west-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
