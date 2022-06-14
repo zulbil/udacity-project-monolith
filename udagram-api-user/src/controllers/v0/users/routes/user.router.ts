0369625654f0eba55f657542562bd28f3a20481c
Zulbil <alexkhang25@yahoo.fr>
2022-06-14 00:33:58 -0500

    update

--- udagram-api-user/src/controllers/v0/users/routes/user.router.ts
+++ udagram-api-user/src/controllers/v0/users/routes/user.router.ts
@@ -2,6 +2,7 @@ import {Router, Request, Response} from 'express';
 
 import {User} from '../models/User';
 import {AuthRouter} from './auth.router';
+import { v4 as uuidv4 } from 'uuid';
 
 const router: Router = Router();
 
@@ -9,10 +10,25 @@ router.use('/auth', AuthRouter);
 
 router.get('/');
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms) );
+}
+
 router.get('/:id', async (req: Request, res: Response) => {
   const {id} = req.params;
   const item = await User.findByPk(id);
   res.send(item);
+});
+
+router.get('/users/:username', async (req: Request, res: Response) => {
+  const {username} = req.params;
+  let pid = uuidv4();
+  let logMessage = `${new Date().toLocaleString()} + : ${pid} - User ${username} requested for resource`;
+  console.log(logMessage);
+  sleep(Math.random()*1000).then(()=> {
+    console.log(logMessage);
+  });
+
 });
 
 export const UserRouter: Router = router;
