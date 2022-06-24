0ed49965623321e3de3765b145b406da48e4da4f
Zulbil <alexkhang25@yahoo.fr>
2022-06-23 21:46:32 -0500

    update

--- udagram-api-user/src/controllers/v0/users/routes/user.router.ts
+++ udagram-api-user/src/controllers/v0/users/routes/user.router.ts
@@ -10,25 +10,24 @@ function sleep(ms: any) {
   return new Promise(resolve => setTimeout(resolve, ms) );
 }
 
-router.get('/:id', async (req: Request, res: Response) => {
-  try {
-    const {id} = req.params;
-    const item = await User.findByPk(id);
-    if(!item) {
-      throw new Error(`User #${id} is not found`);
-    }
-    return res.send(item).status(200);
-  } catch (error: any) {
-    return res.send(error.message).status(400); 
-  }
+// router.get('/:id', async (req: Request, res: Response) => {
+//   try {
+//     const {id} = req.params;
+//     const item = await User.findByPk(id);
+//     if(!item) {
+//       throw new Error(`User #${id} is not found`);
+//     }
+//     return res.send(item).status(200);
+//   } catch (error: any) {
+//     return res.send(error.message).status(400); 
+//   }
   
-});
+// });
 
-router.get('/username/:username', (req: Request, res: Response) => {
+router.get('/:username', (req: Request, res: Response) => {
   const {username} = req.params;
-  console.log(username);
   let pid = uuidv4();
-  let logMessage = `${new Date().toLocaleString()} + : ${pid} - User ${username} requested for resource`;
+  let logMessage = `${new Date().toLocaleString()} : ${pid} - User ${username} requested for resource`;
   console.log(logMessage);
   sleep(Math.random()*1000).then(()=> {
     console.log(logMessage);
@@ -37,6 +36,5 @@ router.get('/username/:username', (req: Request, res: Response) => {
   return res.send({ "message" : logMessage }).status(200);
 });
 
-//router.use('/auth', AuthRouter);
 
 export const UserRouter: Router = router;
