ae7ad09c24655dfe631df3a6357a5ecf06efe554
Zulbil <alexkhang25@yahoo.fr>
2022-06-27 22:02:21 -0500

    update

--- udagram-api-user/src/controllers/v0/users/routes/user.router.ts
+++ udagram-api-user/src/controllers/v0/users/routes/user.router.ts
@@ -10,30 +10,34 @@ function sleep(ms: any) {
   return new Promise(resolve => setTimeout(resolve, ms) );
 }
 
-router.get('/:id', async (req: Request, res: Response) => {
-  try {
-    const {id} = req.params;
-    const item = await User.findByPk(id);
-    if(!item) {
-      throw new Error(`User #${id} is not found`);
-    }
-    return res.send(item).status(200);
-  } catch (error: any) {
-    return res.send(error.message).status(400); 
-  }
-  
-});
-
-router.get('/:username', (req: Request, res: Response) => {
-  const {username} = req.params;
+function logger(username: any) {
   let pid = uuidv4();
   let logMessage = `${new Date().toLocaleString()} : ${pid} - User ${username} requested for resource`;
   console.log(logMessage);
   sleep(Math.random()*1000).then(()=> {
     console.log(logMessage);
   });
+}
 
-  return res.send({ "message" : logMessage }).status(200);
+// router.get('/:id', async (req: Request, res: Response) => {
+//   try {
+//     const {id} = req.params;
+//     const item = await User.findByPk(id);
+//     if(!item) {
+//       throw new Error(`User #${id} is not found`);
+//     }
+//     logger(id); 
+//     return res.send(item).status(200);
+//   } catch (error: any) {
+//     return res.send(error.message).status(400); 
+//   }
+  
+// });
+
+router.get('/:username', (req: Request, res: Response) => {
+  const {username} = req.params;
+  logger(username); 
+  return res.send({ "message" : "Get user "+username }).status(200);
 });
 
 
